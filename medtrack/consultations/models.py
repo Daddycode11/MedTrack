8fbe7f11aea02bb6bf3f43fe8c5b88ea5d808ebd
Roel Christian Yambao <yambao.roelchristian@gmail.com>
2025-04-12 23:22:23 +0800

    Update consultation time block Enum

--- medtrack/consultations/models.py
+++ medtrack/consultations/models.py
@@ -77,56 +77,39 @@ class ConsultationTimeBlock(enum.Enum):
     """
     Enum representing time 30-minute blocks for consultations, listed by start time.
     """
+    BLOCK_08_00 = ("08:00", "8:00 AM")
+    BLOCK_08_30 = ("08:30", "8:30 AM")
+    BLOCK_09_00 = ("09:00", "9:00 AM")
+    BLOCK_09_30 = ("09:30", "9:30 AM")
+    BLOCK_10_00 = ("10:00", "10:00 AM")
+    BLOCK_10_30 = ("10:30", "10:30 AM")
+    BLOCK_11_00 = ("11:00", "11:00 AM")
+    BLOCK_11_30 = ("11:30", "11:30 AM")
+    BLOCK_13_00 = ("13:00", "1:00 PM")
+    BLOCK_13_30 = ("13:30", "1:30 PM")
+    BLOCK_14_00 = ("14:00", "2:00 PM")
+    BLOCK_14_30 = ("14:30", "2:30 PM")
+    BLOCK_15_00 = ("15:00", "3:00 PM")
+    BLOCK_15_30 = ("15:30", "3:30 PM")
+    BLOCK_16_00 = ("16:00", "4:00 PM")
+    BLOCK_16_30 = ("16:30", "4:30 PM")
+    BLOCK_17_00 = ("17:00", "5:00 PM")
 
-    BLOCK_01 = ("00:00", "00:00")
-    BLOCK_02 = ("00:30", "00:30")
-    BLOCK_03 = ("01:00", "01:00")
-    BLOCK_04 = ("01:30", "01:30")
-    BLOCK_05 = ("02:00", "02:00")
-    BLOCK_06 = ("02:30", "02:30")
-    BLOCK_07 = ("03:00", "03:00")
-    BLOCK_08 = ("03:30", "03:30")
-    BLOCK_09 = ("04:00", "04:00")
-    BLOCK_10 = ("04:30", "04:30")
-    BLOCK_11 = ("05:00", "05:00")
-    BLOCK_12 = ("05:30", "05:30")
-    BLOCK_13 = ("06:00", "06:00")
-    BLOCK_14 = ("06:30", "06:30")
-    BLOCK_15 = ("07:00", "07:00")
-    BLOCK_16 = ("07:30", "07:30")
-    BLOCK_17 = ("08:00", "08:00")
-    BLOCK_18 = ("08:30", "08:30")
-    BLOCK_19 = ("09:00", "09:00")
-    BLOCK_20 = ("09:30", "09:30")
-    BLOCK_21 = ("10:00", "10:00")
-    BLOCK_22 = ("10:30", "10:30")
-    BLOCK_23 = ("11:00", "11:00")
-    BLOCK_24 = ("11:30", "11:30")
-    BLOCK_25 = ("12:00", "12:00")
-    BLOCK_26 = ("12:30", "12:30")
-    BLOCK_27 = ("13:00", "13:00")
-    BLOCK_28 = ("13:30", "13:30")
-    BLOCK_29 = ("14:00", "14:00")
-    BLOCK_30 = ("14:30", "14:30")
-    BLOCK_31 = ("15:00", "15:00")
-    BLOCK_32 = ("15:30", "15:30")
-    BLOCK_33 = ("16:00", "16:00")
-    BLOCK_34 = ("16:30", "16:30")
-    BLOCK_35 = ("17:00", "17:00")
-    BLOCK_36 = ("17:30", "17:30")
-    BLOCK_37 = ("18:00", "18:00")
-    BLOCK_38 = ("18:30", "18:30")
-    BLOCK_39 = ("19:00", "19:00")
-    BLOCK_40 = ("19:30", "19:30")
-    BLOCK_41 = ("20:00", "20:00")
-    BLOCK_42 = ("20:30", "20:30")
-    BLOCK_43 = ("21:00", "21:00")
-    BLOCK_44 = ("21:30", "21:30")
-    BLOCK_45 = ("22:00", "22:00")
-    BLOCK_46 = ("22:30", "22:30")
-    BLOCK_47 = ("23:00", "23:00")
-    BLOCK_48 = ("23:30", "23:30")
+    @classmethod
+    def get_block_by_time(cls, time_str):
+        """Get enum member by time string."""
+        for block in cls:
+            if block.value[0] == time_str:
+                return block.name
+        return None
 
+    @classmethod
+    def get_display_time(cls, block_name):
+        """Get display time from block name."""
+        try:
+            return cls[block_name].value[1]
+        except KeyError:
+            return None
 
 class Consultation(models.Model):
     """
@@ -138,7 +121,11 @@ class Consultation(models.Model):
     reason = models.ForeignKey(ConsultationReason, on_delete=models.CASCADE)
     status = models.CharField(max_length=20, choices=[('scheduled', 'Scheduled'), ('completed', 'Completed'), ('canceled', 'Canceled')], default='scheduled')
     consultation_date_date_only = models.DateField(default=None)
-    consultation_time_block = models.CharField(max_length=20, choices=[(block.name, block.value[1]) for block in ConsultationTimeBlock], default=ConsultationTimeBlock.BLOCK_01.name)
+    consultation_time_block = models.CharField(
+        max_length=20, 
+        choices=[(block.name, block.value[1]) for block in ConsultationTimeBlock],
+        default=ConsultationTimeBlock.BLOCK_08_00.name  # Change this line
+    )
     is_an_emergency = models.BooleanField(default=False)
     notes = models.TextField(blank=True, null=True)
 
